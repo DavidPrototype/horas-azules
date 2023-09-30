ce72f40f2e10c1c26a63ccebe57f300c23380369
David Sanchez <dsanchez@prototype.mx>
2023-09-29 18:06:11 -0600

    build 6 rutas imgs app

---
+++ js/preguntasFrecuentes-6d9909de.js
@@ -0,0 +1,1 @@
+import"./main-fe806656.js";gsap.registerPlugin(ScrollTrigger);const s=(r,t)=>{let e=gsap.utils.toArray(r);e.forEach(()=>{gsap.fromTo(e,{yPercent:30,opacity:0},{yPercent:0,opacity:1,duration:1,ease:"ease",scrollTrigger:{trigger:t,start:"-=200 center",end:"top center",scrub:2.5,toggleActions:"restart pause reverse pause"}})})};s(".faqs > *",".faqs");
